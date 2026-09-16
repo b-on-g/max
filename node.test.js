e1c2a7c88da1b9882151a2a96de2887c963a987d
Dev-cmyser <105227884+Dev-cmyser@users.noreply.github.com>
2026-09-16 18:00:51 +0000

    Deploying to gh-pages from @ b-on-g/max@3343f282c99aef2c4625df83190030946aa27254 🚀

--- node.test.js
+++ node.test.js
@@ -20232,8 +20232,8 @@ var $;
 var $;
 (function ($) {
     class $bog_max_ticket extends $giper_baza_dict.with({
-        House: $giper_baza_atom_link_to(() => $bog_max_house),
-        Category: $giper_baza_atom_link_to(() => $bog_max_category),
+        House: $giper_baza_atom_link.to(() => $bog_max_house),
+        Category: $giper_baza_atom_link.to(() => $bog_max_category),
         Place: $giper_baza_atom_text,
         Text: $giper_baza_atom_text,
         Author: $giper_baza_atom_text,
@@ -20291,9 +20291,9 @@ var $;
 (function ($) {
     class $bog_max_uk extends $giper_baza_dict.with({
         Title: $giper_baza_atom_text,
-        Houses: $giper_baza_list_link_to(() => $bog_max_house),
-        Categories: $giper_baza_list_link_to(() => $bog_max_category),
-        Tickets: $giper_baza_list_link_to(() => $bog_max_ticket),
+        Houses: $giper_baza_list_link.to(() => $bog_max_house),
+        Categories: $giper_baza_list_link.to(() => $bog_max_category),
+        Tickets: $giper_baza_list_link.to(() => $bog_max_ticket),
         Bindings: $giper_baza_dict_to($giper_baza_atom_text),
         Notified: $giper_baza_dict_to($giper_baza_atom_text),
     }) {
