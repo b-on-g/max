21644699bbe3a72fece71c4fe37977ffb6248974
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-09-16 20:45:11 +0300

    $bog_max_app: тесты берут класс из $$

--- app/app.test.ts
+++ app/app.test.ts
@@ -3,19 +3,19 @@ namespace $ {
 	$mol_test({
 
 		'home shows the bot error instead of the list'( $ ) {
-			const app = $bog_max_app.make({ $ })
+			const app = $$.$bog_max_app.make({ $ })
 			app.session = ()=> $mol_fail( new Error( 'Подпись MAX не прошла проверку' ) )
 			$mol_assert_equal( app.fail(), 'Подпись MAX не прошла проверку' )
 			$mol_assert_equal( app.home_body(), [ app.Fail() ] )
 		},
 
 		'new ticket needs house, category and place'( $ ) {
-			const app = $bog_max_app.make({ $ })
+			const app = $$.$bog_max_app.make({ $ })
 			app.house = ()=> 'house'
 			app.category = ()=> 'category'
 			app.place = ()=> '  '
 			$mol_assert_equal( app.submit_allowed(), false )
-			const ready = $bog_max_app.make({ $ })
+			const ready = $$.$bog_max_app.make({ $ })
 			ready.house = ()=> 'house'
 			ready.category = ()=> 'category'
 			ready.place = ()=> 'подъезд 2'
@@ -23,7 +23,7 @@ namespace $ {
 		},
 
 		'ticket page opens by arg'( $ ) {
-			const app = $bog_max_app.make({ $ })
+			const app = $$.$bog_max_app.make({ $ })
 			app.ticket_link = ()=> 'link'
 			app.screen = ()=> ''
 			$mol_assert_equal( app.pages(), [ app.Home(), app.Ticket() ] )
